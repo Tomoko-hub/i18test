98163e6a40bb1ab0a3b907d4ad10575462b93b92
Tomoko <haisaitomoco@gmail.com>
2021-10-13 13:53:21 +0300

    create 2buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-2">
+      <h4>Hello</h4>
+      <div >
+        <button type="button" class="btn btn-success">Success</button>
+        <button type="button" class="btn btn-success ml-2">Success</button>
+      </div>
     </div>
   );
 }
